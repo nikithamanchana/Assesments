a6a717529d999948855fc6d366f44cb3cd2f62f9
nikithamanchana <71967208+nikithamanchana@users.noreply.github.com>
2021-05-03 19:26:53 +0530

    Add files via upload

---
+++ Spring/programs/Internship.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+	 
+	public class Internship {
+	 
+	    public static void main(String[] args) {
+	   
+	 
+	        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+	        DXC d = (DXC) ac.getBean("dxc");
+
+	        d.printDXC();
+	       
+	}
+	}
+
